61467b22c141e6ad5fed9599d2607adfd25afec4
Hannes Rabo <hannes.rabo@gmail.com>
2019-09-04 10:01:27 +0200

    Added more instructions

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
-
 setup(
     name='cassandra-slo',
     version='1.0',
     description='Module for setting up virtual network of Cassandra hosts',
-    author='',
-    author_email='',
+    author='Hannes Rabo, Julius Celik, Pethrus Gärdbom, Kamil Nasr, Shubhanker Singh, Vinayak Tejankar',
+    author_email='hrabo@kth.se, jcelik@kth.se, gardborn@kth.se, knasr@kth.se, shusin@kth.se, tejankar@kth.se',
     packages=['cassandra-slo'],
     scripts=['tools/bin/pttestscript']
 )
