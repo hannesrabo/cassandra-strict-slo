418a1074c567a1e4610067f7f55a90ae209c5b91
Hannes Rabo <hannes.rabo@gmail.com>
2019-09-05 13:01:26 +0200

    setup: Added git hooks. (#6)
    * Added git hooks
    * Added a more complete regular expression

--- tools/install-dev.sh
+++ tools/install-dev.sh
@@ -4,8 +4,12 @@
 sudo apt install python3 python3-pip python3-venv
 
 # Creating a virtual environment.
-mkdir .env
-python3 -m venv .env/slo
-source .env/slo/bin/activate
+mkdir env-slo && python3 -m venv env-slo
+source env-slo/bin/activate
 
+# Setting up dependencies
 pip3 install -r requirements.txt 
+
+# Setting up githooks
+REPO_PATH=$(git rev-parse --show-toplevel)
+ln -s "$REPO_PATH/tools/pre-commit.sh" "$REPO_PATH/.git/hooks/pre-commit"
