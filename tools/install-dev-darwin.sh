8481f9864120cb98b63c88cc3074d04c5dba4a6b
Unknown <julius.cc@hotmail.com>
2019-09-05 14:37:15 +0200

    chore: Adds mac install script

---
+++ tools/install-dev-darwin.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Creating a virtual environment.
+mkdir env-slo && python3 -m venv env-slo
+source env-slo/bin/activate
+
+# Setting up dependencies
+pip3 install -r requirements.txt 
+
+# Setting up githooks
+REPO_PATH=$(git rev-parse --show-toplevel)
+ln -s "$REPO_PATH/tools/pre-commit.sh" "$REPO_PATH/.git/hooks/pre-commit"
