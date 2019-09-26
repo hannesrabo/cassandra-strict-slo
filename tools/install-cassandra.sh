be953a6e6584f7369ffa718d44d34425d9c71bad
SLO <slo@kth.se>
2019-09-26 14:48:08 +0000

    Added amazing cassandra configuration

--- tools/install-cassandra.sh
+++ tools/install-cassandra.sh
@@ -5,7 +5,7 @@ REPO_PATH=$(git rev-parse --show-toplevel)
 cd "$REPO_PATH/.."
 
 # Cloning the cassandra fork
-git clone https://github.com/hannesrabo/cassandra.git
+# git clone https://github.com/hannesrabo/cassandra.git
 cd cassandra
 CLONED_PATH=$(pwd)
 
@@ -15,5 +15,9 @@ mkdir -p /slo/cassandra/co
 mkdir -p /slo/cassandra/saved_caches
 
 # Copy configuration files
-cp -R "$REPO_PATH/tools/res/*" "$CLONED_PATH/conf/"
+cp "$REPO_PATH/tools/res/cassandra.yaml" "$CLONED_PATH/conf/cassandra.yaml"
+cp "$REPO_PATH/tools/res/cassandra-env.sh" "$CLONED_PATH/conf/cassandra-env.sh"
+
+mv "$CLONED_PATH/conf/cassandra.yaml" /slo/cassandra/cassandra.yaml
+ln -s /slo/cassandra/cassandra.yaml "$CLONED_PATH/conf/cassandra.yaml"
 
