96df37a8e15a8ebc69e079f32869cfce61f92517
Hannes Rabo <hannes.rabo@gmail.com>
2019-09-26 10:40:33 +0200

    cassandra: Added config/install for cassandra (#41)
    * Added config/install for cassandra
    * Added start in background

--- cassandra_slo/mininet/topology_slo.py
+++ cassandra_slo/mininet/topology_slo.py
@@ -8,8 +8,6 @@ from mininet.net import Mininet
 from mininet.topo import Topo
 from mininet.link import TCLink
 
-from cassandra_slo.cassandra.cassandra_host import CassandraHost
-
 cassandra_node_1 = 'h1'
 cassandra_node_2 = 'h2'
 cassandra_node_3 = 'h3'
@@ -19,6 +17,8 @@ cassandra_ip_1 = '100.0.0.11'
 cassandra_ip_2 = '100.0.0.12'
 cassandra_ip_3 = '100.0.0.13'
 cassandra_ip_4 = '100.0.0.14'
+
+cassandra_private_dirs = [('/slo/cassandra', '/slo/cassandra/%(name)s')]
 
 
 class MyTopo(Topo):
@@ -47,40 +47,8 @@ class MyTopo(Topo):
         # Give some time for network to set up
         sleep(1)
 
-        self.cassandra1 = CassandraHost(
-            host=self.getHost(cassandra_node_1),
-            name=cassandra_node_1,
-            core=0,
-            seed_nodes="%s,%s" % (cassandra_ip_1, cassandra_ip_2),
-        )
-        self.cassandra2 = CassandraHost(
-            host=self.getHost(cassandra_node_2),
-            name=cassandra_node_2,
-            core=1,
-            seed_nodes="%s,%s" % (cassandra_ip_1, cassandra_ip_2),
-        )
-        self.cassandra3 = CassandraHost(
-            host=self.getHost(cassandra_node_3),
-            name=cassandra_node_3,
-            core=2,
-            seed_nodes="%s,%s" % (cassandra_ip_1, cassandra_ip_2),
-        )
-        self.cassandra4 = CassandraHost(
-            host=self.getHost(cassandra_node_4),
-            name=cassandra_node_4,
-            core=3,
-            seed_nodes="%s,%s" % (cassandra_ip_1, cassandra_ip_2),
-        )
-
-        # Starting seeds nodes.
-        self.cassandra1.start_cassandra_host()
-        self.cassandra2.start_cassandra_host()
-
-        sleep(5)
-
-        # Starting the rest of the nodes.
-        self.cassandra3.start_cassandra_host()
-        self.cassandra4.start_cassandra_host()
+        self.startCassandra('h1')
+        self.startCassandra('h2')
 
         # Make sure that everything is set up
         sleep(1)
@@ -93,14 +61,18 @@ class MyTopo(Topo):
         sw1 = self.addSwitch('s1')
 
         # Add hosts
-        host1 = self.addHost('h1', ip="100.0.0.11/24")
-        host2 = self.addHost('h2', ip="100.0.0.12/24")
-        host3 = self.addHost('h3', ip="100.0.0.13/24")
-        host4 = self.addHost('h4', ip="100.0.0.14/24")
-        host5 = self.addHost('h5', ip="100.0.0.15/24")
-        host6 = self.addHost('h6', ip="100.0.0.16/24")
-        host7 = self.addHost('h7', ip="100.0.0.17/24")
-        host8 = self.addHost('h8', ip="100.0.0.18/24")
+        host1 = self.addHost('h1', ip="100.0.0.11/24",
+                             privateDirs=cassandra_private_dirs)
+        host2 = self.addHost('h2', ip="100.0.0.12/24",
+                             privateDirs=cassandra_private_dirs)
+        host3 = self.addHost('h3', ip="100.0.0.13/24",
+                             privateDirs=cassandra_private_dirs)
+        host4 = self.addHost('h4', ip="100.0.0.14/24",
+                             privateDirs=cassandra_private_dirs)
+        host5 = self.addHost('h5', ip="100.0.0.15/24",
+                             privateDirs=cassandra_private_dirs)
+        host6 = self.addHost('h6', ip="100.0.0.16/24",
+                             privateDirs=cassandra_private_dirs)
 
         # Links
         self.addLink(host1, sw1, delay="0.1ms")
@@ -109,8 +81,6 @@ class MyTopo(Topo):
         self.addLink(host4, sw1, delay="0.1ms")
         self.addLink(host5, sw1, delay="0.1ms")
         self.addLink(host6, sw1, delay="0.1ms")
-        self.addLink(host7, sw1, delay="0.1ms")
-        self.addLink(host8, sw1, delay="0.1ms")
 
     def getNet(self):
         return self.net
@@ -118,9 +88,9 @@ class MyTopo(Topo):
     def getHost(self, name):
         return self.net.get(name)
 
-    #     def startHTTP(self, name):
-    #         host = self.getHost(name)
-    #         host.cmd('python application/web/http_server.py ' + name + ' &')
+    def startCassandra(self, name):
+        host = self.getHost(name)
+        host.cmd('./tools/start-cassandra.sh &')
 
     def enableCLI(self):
         CLI(self.net)
