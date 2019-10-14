9aa48c9410982ad7167aa091dbc4cd6c07f32f90
Unknown <julius.cc@hotmail.com>
2019-10-14 13:23:50 +0200

    chore: restructure python code

--- cassandra_slo/mininet/__init__.py
+++
@@ -1,9 +0,0 @@
-def test_func(parameter_list):
-    """documentation"""
-    print("stuff")
-    print("Hej")
-    print("hej hej")
-    print("Hej")
-    print("Last cachange")
-    print("This is test")
-    print("THis is the line")
