79a99c0ffc5a19be6bd6663cd2ce434f80996b6e
Hannes Rabo <hannes.rabo@gmail.com>
2019-11-12 15:29:17 +0100

    Refactored the project

--- tools/parse_ycsb_stderr.py
+++ tools/parse_ycsb_stderr.py
@@ -15,18 +15,22 @@ file = open(rfile, "r")
 lines = list(map(lambda line: line.rstrip('\n'), file.readlines()))
 
 # Remove everything before "Starting test"
+
+
 def get_line_with_find(lines, regex):
     for index, line in enumerate(lines):
         if line.find('Starting test') != -1:
             return index
     return -1
-        
+
+
 body_index = get_line_with_find(lines, 'Starting test.')
 
 body = lines[body_index + 1:]
 
 # Get only data lines
-regex_pattern = re.compile("^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]:[0-9]{3}")
+regex_pattern = re.compile(
+    "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]) (2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]:[0-9]{3}")
 data_lines = list(filter(lambda line: regex_pattern.match(line), body))
 
 # Get first and last line
@@ -40,4 +44,3 @@ end_time_string = last_line.split()[0] + ' ' + last_line.split()[1][:8]
 # Print
 print(start_time_string)
 print(end_time_string)
-
--- tools/slo_graph.py
+++ tools/slo_graph.py
@@ -6,43 +6,48 @@ import sys
 
 
 parsed_file = ""
+
+
 def slo_parse(filename):
     global parsed_file
     filepath = filename
-    percentileth=[]
+    percentileth = []
     latval = []
     opcode = []
     with open(filepath) as fp:
-       line = fp.readline()
-       cnt = 1
-       while line:
-           #print("Lines is: {}".format(line.strip()))
-           line = fp.readline()
-           linestr = line.strip().split(',')
-           if len(linestr)>1 and str(linestr).find("CLEANUP")==-1:
-               #print "linestr[1]:"+linestr[1]
-               if "PercentileLatency" in str(linestr[1]):
-                   latval.append(linestr[2])
-                   percentile = str(linestr[1]).strip().split("PercentileLatency")
-                   #print "percentile:"+percentile[0].replace('th','')
-                   percentileth.append(percentile[0].replace('th',''))
-                   opcode.append(linestr[0])
-           #print len(linestr)
+        line = fp.readline()
+        cnt = 1
+        while line:
+            #print("Lines is: {}".format(line.strip()))
+            line = fp.readline()
+            linestr = line.strip().split(',')
+            if len(linestr) > 1 and str(linestr).find("CLEANUP") == -1:
+                # print "linestr[1]:"+linestr[1]
+                if "PercentileLatency" in str(linestr[1]):
+                    latval.append(linestr[2])
+                    percentile = str(linestr[1]).strip().split(
+                        "PercentileLatency")
+                    # print "percentile:"+percentile[0].replace('th','')
+                    percentileth.append(percentile[0].replace('th', ''))
+                    opcode.append(linestr[0])
+            # print len(linestr)
 
-    #print percentileth
-    #print latval
+    # print percentileth
+    # print latval
 
-    with open("/home/csd/cassandra-strict-slo/"+filename+"_parsed_output","w") as filehandle:
+    with open("/home/csd/cassandra-strict-slo/"+filename+"_parsed_output", "w") as filehandle:
         opcode_test = opcode[0]
         filehandle.write('%s \n' % str(opcode[0]))
-        for valPer,valLat,valop in zip(percentileth,latval,opcode):
+        for valPer, valLat, valop in zip(percentileth, latval, opcode):
             if valop != opcode_test:
                 filehandle.write('\n %s \n' % str(valop))
                 opcode_test = valop
-            filehandle.write('%s,%s\n' % (str(valPer),str(valLat)))
+            filehandle.write('%s,%s\n' % (str(valPer), str(valLat)))
         filehandle.close()
     parsed_file = "/home/csd/cassandra-strict-slo/"+filename+"_parsed_output"
-    print ("Parsed output file available at /home/csd/cassandra-strict-slo/"+filename+"_parsed_output")
+    print("Parsed output file available at /home/csd/cassandra-strict-slo/" +
+          filename+"_parsed_output")
+
 
 def slo_plot(filename):
     global parsed_file
@@ -53,36 +58,37 @@ def slo_plot(filename):
     y_b = []
     operation = []
     temp = 0
-    with open(parsed_file,'r') as fp:
+    with open(parsed_file, 'r') as fp:
         line = fp.readline()
         while line:
             linestr = line.strip().split(',')
-            if str(line).find(']')!=-1:
-                #print str(line)
+            if str(line).find(']') != -1:
+                # print str(line)
                 operation.append(str(line))
                 temp = temp + 1
-                #print temp
-            if len(linestr)>1 and temp%2==1:
+                # print temp
+            if len(linestr) > 1 and temp % 2 == 1:
                 x_a.append(float(linestr[1]))
                 y_a.append(float(linestr[0]))
-            elif len(linestr)>1 and temp%2==0:
+            elif len(linestr) > 1 and temp % 2 == 0:
                 x_b.append(float(linestr[1]))
                 y_b.append(float(linestr[0]))
             line = fp.readline()
-    plt.plot(x_a,y_a, label=str(operation[0]))
-    plt.plot(x_b,y_b, label=str(operation[1]))
+    plt.plot(x_a, y_a, label=str(operation[0]))
+    plt.plot(x_b, y_b, label=str(operation[1]))
     plt.xlabel('Latency (us)')
     plt.ylabel('Request Percentile')
     plt.title('SLO Tail latency for Cassandra')
     plt.legend()
     plt.show()
 
+
 if __name__ == "__main__":
-     if len(sys.argv)<2:
-        print ("Please enter file name as argument 1 and to Plot 'plot' as the 2nd argument")
+    if len(sys.argv) < 2:
+        print("Please enter file name as argument 1 and to Plot 'plot' as the 2nd argument")
         exit()
-     else:
-        #print len(sys.argv)
+    else:
+        # print len(sys.argv)
         slo_parse(str(sys.argv[1]))
-        if len(sys.argv)>2:
-           slo_plot(str(sys.argv[1]))
+        if len(sys.argv) > 2:
+            slo_plot(str(sys.argv[1]))
