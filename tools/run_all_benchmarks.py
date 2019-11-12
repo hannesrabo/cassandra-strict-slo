28d2850351a3fc3683368e93d30a09a39ce555ab
Unknown <julius.cc@hotmail.com>
2019-11-12 15:03:17 +0100

    Add speculative retries in benchmark

--- tools/run_all_benchmarks.py
+++ tools/run_all_benchmarks.py
@@ -7,14 +7,16 @@ import numpy as np
 
 
 if len(sys.argv) <= 1:
-    print ("Please enter a filename prefix")
+    print("Please enter a filename prefix")
     exit()
 
 filename_prefix = sys.argv[1]
 operation_targets = np.arange(400, 3600, 200).tolist()
+sr_thresholds = np.arange(1, 20, 1).tolist()
 
-for target in operation_targets:
-    filename = filename_prefix + "_" + str(target)
-    print ("Running benchmark for file: " + filename)
-    os.system("./tools/run_workload.py " + filename + " " + str(target))
-
+for threshold in sr_thresholds:
+    for target in operation_targets:
+        filename = filename_prefix + "_" + str(threshold) + "_" + str(target)
+        print("Running benchmark for file: " + filename)
+        os.system("./tools/run_workload.py " + filename +
+                  " " + str(threshold) + " " + str(target))
